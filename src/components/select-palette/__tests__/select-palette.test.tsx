e4df368ea09008007b8e0e880b6d4eb47f562400
Andrey Kiselev <omega-faworit@yandex.ru>
2021-03-28 02:16:13 +0300

    testing: update select palette tests

--- src/components/select-palette/__tests__/select-palette.test.tsx
+++ src/components/select-palette/__tests__/select-palette.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { SelectPalette } from '../select-palette';
 
 jest.mock('../../../hooks/useKeyMap', () => ({
@@ -15,37 +15,6 @@ const onCloseMock = jest.fn();
 const onSelectMock = jest.fn();
 
 const exampleOptions: string[] = ['option1', 'option2', 'option3'];
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-function triggerKeyboardEvent(el, keyCode) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const eventObj = document.createEventObject
-    ? // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      document.createEventObject()
-    : document.createEvent('Events');
-  if (eventObj.initEvent) {
-    eventObj.initEvent('keydown', true, true);
-  }
-  if (keyCode) {
-    eventObj.keyCode = keyCode;
-    eventObj.which = keyCode;
-  }
-  // if (opt) {
-  //   // eslint-disable-next-line no-restricted-syntax
-  //   for (const a in opt) {
-  //     if (Object.prototype.hasOwnProperty.call(opt, a)) {
-  //       eventObj[a] = opt[a];
-  //     }
-  //   }
-  // }
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  el.dispatchEvent
-    ? el.dispatchEvent(eventObj)
-    : el.fireEvent('onkeydown', eventObj);
-}
 
 describe('SelectPalette', () => {
   beforeEach(() => {
@@ -120,32 +89,6 @@ describe('SelectPalette', () => {
     expect(options).toHaveLength(exampleOptions.length);
     expect(option.getAttribute('aria-selected')).toEqual('false');
     expect(option2.getAttribute('aria-selected')).toEqual('true');
-    expect(onCloseMock).not.toBeCalled();
-    expect(onSelectMock).toBeCalledTimes(1);
-    expect(onSelectMock).toBeCalledWith(exampleOptions[1]);
-  });
-
-  it('should select next item on down key', async () => {
-    const component = render(
-      <SelectPalette
-        isOpen
-        onClose={onCloseMock}
-        onSelect={onSelectMock}
-        options={exampleOptions}
-      />
-    );
-
-    const options = component.getAllByRole('option');
-    const option = options[0];
-    const option2 = options[1];
-
-    expect(options).toHaveLength(exampleOptions.length);
-
-    triggerKeyboardEvent(component.container, 'b');
-    await waitFor(() =>
-      expect(option2.getAttribute('aria-selected')).toEqual('true')
-    );
-    expect(option.getAttribute('aria-selected')).toEqual('false');
     expect(onCloseMock).not.toBeCalled();
     expect(onSelectMock).toBeCalledTimes(1);
     expect(onSelectMock).toBeCalledWith(exampleOptions[1]);
