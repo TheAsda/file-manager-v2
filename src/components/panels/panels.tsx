5ee4338d032b6b1f3577760b7b159d6a3972282b
Andrey Kiselev <omega-faworit@yandex.ru>
2021-04-17 00:39:41 +0300

    feat: add usePanels

--- src/components/panels/panels.tsx
+++ src/components/panels/panels.tsx
@@ -1,153 +1,17 @@
-import React, { useMemo, useRef, useState } from 'react';
-import { useHotkeys } from 'react-hotkeys-hook';
+import React from 'react';
 import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex';
-import { join } from 'path';
-import { error } from 'electron-log';
-import { useKeyMap } from '../../hooks/useKeyMap';
-import { Panel, PanelRef } from '../panel/panel';
-import { Command } from '../../types/command';
-import { useRegisterCommands } from '../../hooks/useCommands';
-import { useInputModal } from '../../hooks/useInputModal';
-import { createFile, createFolder, rename, trash } from '../../utils/fsActions';
-import { useConfirmDialog } from '../../hooks/useConfirmDialog';
+import { Panel } from '../panel/panel';
+import { usePanels } from '../../hooks/usePanels';
 
 export const Panels = () => {
-  const { switchPanel } = useKeyMap();
-  const { openInputModal, isOpen: inputModalIsOpen } = useInputModal();
-  const { openConfirmDialog, isOpen: confirmDialogIsOpen } = useConfirmDialog();
-  const [focusedPanel, setFocusedPanel] = useState<'left' | 'right'>('left');
-  const leftPanelRef = useRef<PanelRef>();
-  const rightPanelRef = useRef<PanelRef>();
-
-  const currentPanelRef =
-    focusedPanel === 'left' ? leftPanelRef : rightPanelRef;
-  const otherPanelRef = focusedPanel === 'left' ? rightPanelRef : leftPanelRef;
-
-  const updateDirectory = () => {
-    currentPanelRef.current?.updateDirectory?.();
-    if (currentPanelRef.current?.path === otherPanelRef.current?.path) {
-      otherPanelRef.current?.updateDirectory?.();
-    }
-  };
-
-  const togglePanel = () => {
-    setFocusedPanel((s) => {
-      otherPanelRef.current?.currentElement?.focus();
-
-      return s === 'left' ? 'right' : 'left';
-    });
-  };
-
-  const onRename = async (newName: string, oldName: string, path: string) => {
-    await rename(newName, oldName, path);
-    currentPanelRef.current?.updateDirectory?.();
-  };
-
-  useHotkeys(switchPanel, togglePanel, {
-    enabled: !inputModalIsOpen && !confirmDialogIsOpen,
-  });
-
-  const commands = useMemo(() => {
-    return [
-      {
-        name: 'New file',
-        handler: () => {
-          if (currentPanelRef.current === undefined) {
-            return;
-          }
-          const { path, currentElement } = currentPanelRef.current;
-
-          openInputModal({
-            title: 'New file',
-            onComplete: async (value) => {
-              await createFile(value, path);
-              updateDirectory();
-            },
-            elementToFocus: currentElement,
-          });
-        },
-      },
-      {
-        name: 'New folder',
-        handler: () => {
-          if (currentPanelRef.current === undefined) {
-            return;
-          }
-          const { path, currentElement } = currentPanelRef.current;
-
-          openInputModal({
-            title: 'New folder',
-            onComplete: async (value) => {
-              await createFolder(value, path);
-              updateDirectory();
-            },
-            elementToFocus: currentElement,
-          });
-        },
-      },
-      {
-        name: 'Rename',
-        handler: () => {
-          if (currentPanelRef.current === undefined) {
-            return;
-          }
-          const { startRename } = currentPanelRef.current;
-
-          startRename();
-        },
-      },
-      {
-        name: 'Delete',
-        handler: async () => {
-          if (currentPanelRef.current === undefined) {
-            return;
-          }
-          const { currentItems, currentElement } = currentPanelRef.current;
-
-          if (currentItems.length === 0) {
-            const path = join(currentItems[0].path, currentItems[0].name);
-
-            openConfirmDialog({
-              title: 'Are you sure you want to delete this item?',
-              onOk: async () => {
-                try {
-                  await trash(path);
-                  updateDirectory();
-                } catch (err) {
-                  error(err);
-                }
-              },
-              elementToFocus: currentElement,
-            });
-          } else {
-            const paths = currentItems.map((item) =>
-              join(item.path, item.name)
-            );
-
-            openConfirmDialog({
-              title: `Are you sure you want to delete ${paths.length} items`,
-              onOk: async () => {
-                try {
-                  await Promise.all(paths.map((path) => trash(path)));
-                  updateDirectory();
-                } catch (err) {
-                  error(err);
-                }
-              },
-              elementToFocus: currentElement,
-            });
-          }
-        },
-      },
-    ] as Command[];
-  }, [currentPanelRef, openConfirmDialog, openInputModal]);
-
-  useRegisterCommands('panels', commands);
-
-  const leftPanelIsFocused =
-    focusedPanel === 'left' && !inputModalIsOpen && !confirmDialogIsOpen;
-  const rightPanelIsFocused =
-    focusedPanel === 'right' && !inputModalIsOpen && !confirmDialogIsOpen;
+  const {
+    leftPanelRef,
+    rightPanelRef,
+    leftPanelIsFocused,
+    rightPanelIsFocused,
+    onRename,
+    setFocusedPanel,
+  } = usePanels();
 
   return (
     <ReflexContainer orientation="vertical">
---
+++ src/hooks/usePanels.ts
@@ -0,0 +1,163 @@
+import { error } from 'electron-log';
+import { join } from 'path';
+import { useState, useRef, useCallback, useMemo } from 'react';
+import { useHotkeys } from 'react-hotkeys-hook';
+import { PanelRef } from '../components/panel/panel';
+import { Command } from '../types/command';
+import { createFile, createFolder, rename, trash } from '../utils/fsActions';
+import { useRegisterCommands } from './useCommands';
+import { useConfirmDialog } from './useConfirmDialog';
+import { useInputModal } from './useInputModal';
+import { useKeyMap } from './useKeyMap';
+
+export const usePanels = () => {
+  const { switchPanel } = useKeyMap();
+  const { isOpen: inputModalIsOpen, openInputModal } = useInputModal();
+  const { isOpen: confirmDialogIsOpen, openConfirmDialog } = useConfirmDialog();
+  const [focusedPanel, setFocusedPanel] = useState<'left' | 'right'>('left');
+  const leftPanelRef = useRef<PanelRef>();
+  const rightPanelRef = useRef<PanelRef>();
+  const currentPanelRef = useRef<PanelRef>();
+  const otherPanelRef = useRef<PanelRef>();
+
+  currentPanelRef.current =
+    focusedPanel === 'left' ? leftPanelRef.current : rightPanelRef.current;
+  otherPanelRef.current =
+    focusedPanel === 'left' ? rightPanelRef.current : leftPanelRef.current;
+
+  const updateCurrentDirectory = useCallback(() => {
+    currentPanelRef.current?.updateDirectory?.();
+    if (currentPanelRef.current?.path === otherPanelRef.current?.path) {
+      otherPanelRef.current?.updateDirectory?.();
+    }
+  }, []);
+
+  const togglePanel = () => {
+    setFocusedPanel((s) => {
+      otherPanelRef.current?.currentElement?.focus();
+
+      return s === 'left' ? 'right' : 'left';
+    });
+  };
+
+  const onRename = async (newName: string, oldName: string, path: string) => {
+    await rename(newName, oldName, path);
+    currentPanelRef.current?.updateDirectory?.();
+  };
+
+  useHotkeys(switchPanel, togglePanel, {
+    enabled: !inputModalIsOpen && !confirmDialogIsOpen,
+  });
+
+  const commands = useMemo(() => {
+    return [
+      {
+        name: 'New file',
+        handler: () => {
+          if (currentPanelRef.current === undefined) {
+            return;
+          }
+          const { path, currentElement } = currentPanelRef.current;
+
+          openInputModal({
+            title: 'New file',
+            onComplete: async (value) => {
+              await createFile(value, path);
+              updateCurrentDirectory();
+            },
+            elementToFocus: currentElement,
+          });
+        },
+      },
+      {
+        name: 'New folder',
+        handler: () => {
+          if (currentPanelRef.current === undefined) {
+            return;
+          }
+          const { path, currentElement } = currentPanelRef.current;
+
+          openInputModal({
+            title: 'New folder',
+            onComplete: async (value) => {
+              await createFolder(value, path);
+              updateCurrentDirectory();
+            },
+            elementToFocus: currentElement,
+          });
+        },
+      },
+      {
+        name: 'Rename',
+        handler: () => {
+          if (currentPanelRef.current === undefined) {
+            return;
+          }
+          const { startRename } = currentPanelRef.current;
+
+          startRename();
+        },
+      },
+      {
+        name: 'Delete',
+        handler: async () => {
+          if (currentPanelRef.current === undefined) {
+            return;
+          }
+          const { currentItems, currentElement } = currentPanelRef.current;
+
+          if (currentItems.length === 1) {
+            const path = join(currentItems[0].path, currentItems[0].name);
+
+            openConfirmDialog({
+              title: 'Are you sure you want to delete this item?',
+              onOk: async () => {
+                try {
+                  await trash(path);
+                  updateCurrentDirectory();
+                } catch (err) {
+                  error(err);
+                }
+              },
+              elementToFocus: currentElement,
+            });
+          } else {
+            const paths = currentItems.map((item) =>
+              join(item.path, item.name)
+            );
+
+            openConfirmDialog({
+              title: `Are you sure you want to delete ${paths.length} items`,
+              onOk: async () => {
+                try {
+                  await Promise.all(paths.map((path) => trash(path)));
+                  updateCurrentDirectory();
+                } catch (err) {
+                  error(err);
+                }
+              },
+              elementToFocus: currentElement,
+            });
+          }
+        },
+      },
+    ] as Command[];
+  }, [openConfirmDialog, openInputModal, updateCurrentDirectory]);
+
+  useRegisterCommands('panels', commands);
+
+  const leftPanelIsFocused =
+    focusedPanel === 'left' && !inputModalIsOpen && !confirmDialogIsOpen;
+  const rightPanelIsFocused =
+    focusedPanel === 'right' && !inputModalIsOpen && !confirmDialogIsOpen;
+
+  return {
+    leftPanelRef,
+    rightPanelRef,
+    leftPanelIsFocused,
+    rightPanelIsFocused,
+    onRename,
+    updateCurrentDirectory,
+    setFocusedPanel,
+  };
+};
